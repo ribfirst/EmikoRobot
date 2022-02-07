bf752f1686b1b6d87df16680b849136509d8fc2e
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-07 19:14:01 +0530

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -9,16 +9,19 @@ from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/2c13e570944404a6c0d86.jpg"
+PHOTO = "https://telegra.ph/file/060ea554c591fc5b0b878.jpg"
 
-@register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emiko Robot.** \n\n"
-  TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [sena-ex](https://t.me/excrybaby)** \n\n"
-  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
-  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/EmiexRobot?start=help"), Button.url("Support", "https://t.me/emikosupport")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
+    TEXT = "¤¤ 𝐇𝐞𝐲 𝐌𝐚𝐞𝐬𝐭𝐫𝐨, 𝐈'𝐦 𝐔𝐧𝐦𝐞𝐢! ¤¤\n\n"
+    TEXT += f"ø 𝐘𝐞𝐚𝐡!! 𝐃𝐨𝐧'𝐭 𝐛𝐨𝐭𝐡𝐞𝐫 𝐚𝐛𝐨𝐮𝐭 𝐦𝐲 𝐬𝐩𝐞𝐞𝐝 𝐟𝐨𝐫 𝐧𝐨𝐰 ø\n\n"
+    TEXT += f"ø 𝐔𝐍𝐌𝐄𝐈 : 𝐋𝐀𝐓𝐄𝐒𝐓 𝐕𝐄𝐑𝐒𝐈𝐎𝐍 ø\n\n"
+    TEXT += f"ø 𝐌𝐲 𝐌𝐚𝐞𝐬𝐭𝐫𝐨 (𝐂𝐫𝐞𝐚𝐭𝐨𝐫) : [ 𝐒𝐡𝐨𝐭𝐨](http://t.me/yameteee_yamete_kudasai) ø\n\n"
+    TEXT += f"ø ᴀɴʏ ɪ𝓼𝓼ᴜᴇ𝓼 ᴄᴏɴᴛᴀᴄᴛ us ʜᴇʀᴇ : @unmei_support ø\n\n"
+    TEXT += "♥ ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ♥ (っ◔◡◔)っ "
+    BUTTON = [
+        [
+            Button.url("📢 Updates", "https://t.me/unmei_updates"),
+            Button.url("🚑 Support", "https://t.me/unmei_support"),
+        ]
+    ]
+    await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
