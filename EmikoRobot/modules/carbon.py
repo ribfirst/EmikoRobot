bac66f6d3866a7e5eb3248edc941aba1510196ea
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-07 19:26:48 +0530

    Update carbon.py

--- EmikoRobot/modules/carbon.py
+++ EmikoRobot/modules/carbon.py
@@ -24,15 +24,15 @@ async def carbon_func(_, message):
     carbon.close()
 
 
-MEMEK = "https://telegra.ph/file/11b5922a33de9968cedfe.jpg"
+MEMEK = "https://telegra.ph/file/a4f96c30605ece22664ea.jpg"
 
 @pbot.on_message(filters.command("repo"))
 async def repo(_, message):
     await message.reply_photo(
         photo=MEMEK,
-        caption=f"""✨ **Hey I'm Emiko Robot** 
+        caption=f"""✨ **Hey I'm Unmei Bot** 
 
-**Owner repo : [sena-ex](https://t.me/excrybaby)**
+**Owner : [Shoto](https://t.me/yameteee_yamete_kudasai)**
 **Python Version :** `{y()}`
 **Library Version :** `{o}`
 **Telethon Version :** `{s}`
@@ -44,9 +44,9 @@ async def repo(_, message):
             [
                 [
                     InlineKeyboardButton(
-                        "Repo", url="https://github.com/kennedy-ex/emikorobot"), 
+                        "Repo", url="https://github.com/DarkSoulxUltra/EmikoRobot"), 
                     InlineKeyboardButton(
-                        "Support", url="https://t.me/emikosupport")
+                        "Support", url="https://t.me/unmei_support")
                 ]
             ]
         )
