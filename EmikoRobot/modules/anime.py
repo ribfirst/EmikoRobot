77966bda664bce6c8417cd54a2fc94a6a171f053
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-12 10:02:56 +0530

    Update anime.py

--- EmikoRobot/modules/anime.py
+++ EmikoRobot/modules/anime.py
@@ -156,11 +156,7 @@ query ($id: Int,$search: String) {
     }
 """
 
-AWAKE_MSG = "**✩✮ ɪ ᴀᴍ ᴜɴᴍᴇɪ, ᴀ ᴘᴏᴡᴇʀꜰᴜʟ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ. **\n\n
-**✩✮ I'm working fine as up now 👌︎.**\n\n
-**✩✮ Unmei: Version 1.2**\n\n
-**✩✮ ᴀɴʏ ɪssᴜᴇs ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ @unmei_support **\n\n
-**✩✮ 💞 ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ 💞**"
+AWAKE_MSG = "**✩✮ ɪ ᴀᴍ ᴜɴᴍᴇɪ, ᴀ ᴘᴏᴡᴇʀꜰᴜʟ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ. **\n\n**✩✮ I'm working fine as up now 👌︎.**\n\n**✩✮ Unmei: Version 1.2**\n\n**✩✮ ᴀɴʏ ɪssᴜᴇs ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ @unmei_support **\n\n**✩✮ 💞 ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ 💞**"
 
 url = 'https://graphql.anilist.co'
 
