3d1f5933a12ef619f408cb6bff19ee706c592d0f
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-12 14:53:31 +0530

    Update google.py

--- EmikoRobot/modules/google.py
+++ EmikoRobot/modules/google.py
@@ -7,11 +7,12 @@ import re
 import aiohttp
 import urllib.request
 from urllib.parse import urlencode
+from urllib.error import URLError, HTTPError
 import requests
 from bs4 import BeautifulSoup
 from PIL import Image
 from search_engine_parser import GoogleSearch
-
+from telegram.ext import CallbackContext, run_async
 import bs4
 import html2text
 from bing_image_downloader import downloader
@@ -21,9 +22,10 @@ from telethon.tl import types
 from telethon.tl.types import *
 
 from EmikoRobot import *
-
+from EmikoRobot.modules.disable import DisableAbleCommandHandler
 from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
+from EmikoRobot.modules.helper_funcs.alternate import typing_action
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
@@ -91,76 +93,114 @@ useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) 
 opener.addheaders = [("User-agent", useragent)]
 
 
-@register(pattern=r"^/reverse|grs|pp (?: |$)(\d*)")
-async def okgoogle(img):
-    """For .reverse command, Google search images and stickers."""
+async def reverse(update: Update, context:CallbackContext):
     if os.path.isfile("okgoogle.png"):
         os.remove("okgoogle.png")
 
-    message = await img.get_reply_message()
-    if message and message.media:
-        photo = io.BytesIO()
-        await tbot.download_media(message, photo)
+    msg = update.effective_message
+    chat_id = update.effective_chat.id
+    bot, args = context.bot, context.args
+    rtmid = msg.message_id
+    imagename = "okgoogle.png"
+
+    reply = msg.reply_to_message
+    if reply:
+        if reply.sticker:
+            file_id = reply.sticker.file_id
+        elif reply.photo:
+            file_id = reply.photo[-1].file_id
+        elif reply.document:
+            file_id = reply.document.file_id
+        else:
+            msg.reply_text("Reply to an image or sticker to lookup.")
+            return
+        image_file = bot.get_file(file_id)
+        image_file.download(imagename)
+        if args:
+            txt = args[0]
+            try:
+                lim = int(txt)
+            except:
+                lim = 2
+        else:
+            lim = 2
+    elif args and not reply:
+        splatargs = msg.text.split(" ")
+        if len(splatargs) == 3:                
+            img_link = splatargs[1]
+            try:
+                lim = int(splatargs[2])
+            except:
+                lim = 2
+        elif len(splatargs) == 2:
+            img_link = splatargs[1]
+            lim = 2
+        else:
+            msg.reply_text("/reverse <link> <amount of images to return.>")
+            return
+        try:
+            urllib.request.urlretrieve(img_link, imagename)
+        except HTTPError as HE:
+            if HE.reason == 'Not Found':
+                msg.reply_text("Image not found.")
+                return
+            elif HE.reason == 'Forbidden':
+                msg.reply_text("Couldn't access the provided link, The website might have blocked accessing to the website by bot or the website does not existed.")
+                return
+        except URLError as UE:
+            msg.reply_text(f"{UE.reason}")
+            return
+        except ValueError as VE:
+            msg.reply_text(f"{VE}\nPlease try again using http or https protocol.")
+            return
     else:
-        await img.reply("`Reply to photo or sticker nigger.`")
+        msg.reply_markdown("Please reply to a sticker, or an image to search it!\nDo you know that you can search an image with a link too? /reverse [picturelink] <amount>.")
         return
 
-    if photo:
-        dev = await img.reply("`Processing...`")
-        try:
-            image = Image.open(photo)
-        except OSError:
-            await dev.edit("`Unsupported sexuality, most likely.`")
-            return
-        name = "okgoogle.png"
-        image.save(name, "PNG")
-        image.close()
-        # https://stackoverflow.com/questions/23270175/google-reverse-image-search-using-post-request#28792943
-        searchUrl = "https://www.google.com/searchbyimage/upload"
-        multipart = {"encoded_image": (name, open(name, "rb")), "image_content": ""}
+    try:
+        searchUrl = 'https://www.google.com/searchbyimage/upload'
+        multipart = {'encoded_image': (imagename, open(imagename, 'rb')), 'image_content': ''}
         response = requests.post(searchUrl, files=multipart, allow_redirects=False)
-        fetchUrl = response.headers["Location"]
+        fetchUrl = response.headers['Location']
 
         if response != 400:
-            await dev.edit(
-                "`Image successfully uploaded to Google. Maybe.`"
-                "\n`Parsing source now. Maybe.`"
-            )
+            xx = bot.send_message(chat_id, "Image was successfully uploaded to Google."
+                                  "\nParsing it, please wait.", reply_to_message_id=rtmid)
         else:
-            await dev.edit("`Google told me to fuck off.`")
+            xx = bot.send_message(chat_id, "Google told me to go away.", reply_to_message_id=rtmid)
             return
 
-        os.remove(name)
-        match = await ParseSauce(fetchUrl + "&preferences?hl=en&fg=1#languages")
-        guess = match["best_guess"]
-        imgspage = match["similar_images"]
+        os.remove(imagename)
+        match = ParseSauce(fetchUrl + "&hl=en")
+        guess = match['best_guess']
+        if match['override'] and not match['override'] == '':
+            imgspage = match['override']
+        else:
+            imgspage = match['similar_images']
 
         if guess and imgspage:
-            await dev.edit(f"[{guess}]({fetchUrl})\n\n`Looking for this Image...`")
+            xx.edit_text(f"[{guess}]({fetchUrl})\nProcessing...", parse_mode='Markdown', disable_web_page_preview=True)
         else:
-            await dev.edit("`Can't find this piece of shit.`")
+            xx.edit_text("Couldn't find anything.")
             return
 
-        if img.pattern_match.group(1):
-            lim = img.pattern_match.group(1)
-        else:
-            lim = 3
-        images = await scam(match, lim)
-        yeet = []
-        for i in images:
-            k = requests.get(i)
-            yeet.append(k.content)
-        try:
-            await tbot.send_file(
-                entity=await tbot.get_input_entity(img.chat_id),
-                file=yeet,
-                reply_to=img,
-            )
-        except TypeError:
-            pass
-        await dev.edit(
-            f"[{guess}]({fetchUrl})\n\n[Visually similar images]({imgspage})"
-        )
+        images = scam(imgspage, lim)
+        if len(images) == 0:
+            xx.edit_text(f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})"
+                          "\nCouldn't fetch any images.", parse_mode='Markdown', disable_web_page_preview=True)
+            return
+
+        imglinks = []
+        for link in images:
+            lmao = InputMediaPhoto(media=str(link))
+            imglinks.append(lmao)
+
+        bot.send_media_group(chat_id=chat_id, media=imglinks, reply_to_message_id=rtmid)
+        xx.edit_text(f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})", parse_mode='Markdown', disable_web_page_preview=True)
+    except TelegramError as e:
+        print(e)
+    except Exception as exception:
+        print(exception)
 
 
 async def ParseSauce(googleurl):
@@ -209,7 +249,6 @@ async def scam(results, lim):
 
 @register(pattern="^/app (.*)")
 async def apk(e):
-
     try:
         app_name = e.pattern_match.group(1)
         remove_space = app_name.split(" ")
@@ -291,3 +330,9 @@ __help__ = """
 
   💡Ex: `Unmei where is Japan?`
 """
+
+REVERSE_HANDLER = DisableAbleCommandHandler(
+    ["reverse","grs","pp"], reverse, parse_args=True, admin_ok=True
+)
+dispatcher.add_handler(REVERSE_HANDLER)
+
