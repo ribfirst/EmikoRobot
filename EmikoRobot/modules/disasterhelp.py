f32958b7694ed2da63bc166f83715e723a67735f
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-14 22:46:45 +0530

    Create disasterhelp.py

---
+++ EmikoRobot/modules/disasterhelp.py
@@ -0,0 +1,52 @@
+__help__ = """
+*⚠️ Notice:*
+Commands listed here only work for users with special access are mainly used for troubleshooting, debugging purposes.
+Group admins/group owners do not need these commands. 
+ ╔ *List all special users:*
+ ╠ ✧ `/musicarts`*:* Lists all Dragon disasters
+ ╠ ✧ `/d2slayers`*:* Lists all Demon disasters
+ ╠ ✧ `/defenders`*:* Lists all Tigers disasters
+ ╠ ✧ `/mel_creators`*:* Lists all Wolf disasters
+ ╠ ✧ `/conductors`*:* Lists all Hero Association members
+ ╠ ✧ `/addmusicart`*:* Adds a user to Dragon
+ ╠ ✧ `/addd2slayer`*:* Adds a user to Demon
+ ╠ ✧ `/adddefender`*:* Adds a user to Tiger
+ ╠ ✧ `/addmelcr`*:* Adds a user to Wolf
+ ╚ ✧ `Add dev doesnt exist, devs should know how to add themselves`
+ ╔ *Ping:*
+ ╠ ✧ `/ping`*:* gets ping time of bot to telegram server
+ ╚ ✧ `/pingall`*:* gets all listed ping times
+ ╔ *Broadcast: (Bot owner only)*
+ ╠  *Note:* This supports basic markdown
+ ╠ ✧ `/broadcastall`*:* Broadcasts everywhere
+ ╠ ✧ `/broadcastusers`*:* Broadcasts too all users
+ ╚ ✧ `/broadcastgroups`*:* Broadcasts too all groups
+ ╔ *Groups Info:*
+ ╠ ✧ `/groups`*:* List the groups with Name, ID, members count as a txt
+ ╠ ✧ `/leave <ID>`*:* Leave the group, ID must have hyphen
+ ╠ ✧ `/stats`*:* Shows overall bot stats
+ ╠ ✧ `/getchats`*:* Gets a list of group names the user has been seen in. Bot owner only
+ ╚ ✧ `/ginfo username/link/ID`*:* Pulls info panel for entire group
+ ╔ *Access control:* 
+ ╠ ✧ `/ignore`*:* Blacklists a user from 
+ ╠  using the bot entirely
+ ╠ ✧ `/notice`*:* Removes user from blacklist
+ ╚ ✧ `/ignoredlist`*:* Lists ignored users
+ ╔ *Speedtest:*
+ ╚ ✧ `/speedtest`*:* Runs a speedtest and gives you 2 options to choose from, text or image output
+ ╔ *Global Bans:*
+ ╠ ✧ `/gban user reason`*:* Globally bans a user
+ ╚ ✧ `/ungban user reason`*:* Unbans the user from the global bans list
+ ╔ *Windows self hosted only:*
+ ╠ ✧ `/reboot`*:* Restarts the bots service
+ ╚ ✧ `/gitpull`*:* Pulls the repo and then restarts the bots service
+ ╔ *Chatbot:* 
+ ╚ ✧ `/listaichats`*:* Lists the chats the chatmode is enabled in
+ ╔ *Global Bans:*
+ ╠ ✧ `/gban <id> <reason>`*:* Gbans the user, works by reply too
+ ╠ ✧ `/ungban`*:* Ungbans the user, same usage as gban
+ ╚ ✧ `/gbanlist`*:* Outputs a list of gbanned users
+Visit @unmei_support for more information.
+"""
+
+__mod_name__ = "Disasters"
