10973473e84f90615fd60989db203e5dd399c376
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-18 19:05:17 +0530

    Update antinsfw.py

--- EmikoRobot/modules/antinsfw.py
+++ EmikoRobot/modules/antinsfw.py
@@ -150,7 +150,7 @@ async def nsfw_enable_disable(_, message):
     status = status.lower()
     chat_id = message.chat.id
     if status == "on" or status == "yes":
-        nsfw_on(chat_id)
+        await nsfw_on(chat_id)
         await message.reply_text(
             "Enabled AntiNSFW System. I will Delete inappropraite media from Horny people."
         )
